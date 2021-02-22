b96f9724e671e48c8b392d5f5bcdb7347d284491
Saúl Laínez <saullainez@hotmail.es>
2021-02-22 11:28:08 -0600

    Inicio del proyecto

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HeroeComponent } from './pages/heroe/heroe.component';
+import { HeroesComponent } from './pages/heroes/heroes.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'heroes', component: HeroesComponent },
+  { path: 'heroe/:id', component: HeroeComponent },
+  { path: '**', pathMatch: 'full', redirectTo: 'heroes' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
