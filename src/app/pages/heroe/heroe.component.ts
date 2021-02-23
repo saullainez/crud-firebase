9a8e38f08c3c7b40c97ec3dfc6635470c8629d70
Saúl Laínez <saullainez@hotmail.es>
2021-02-23 10:54:10 -0600

    Modelo para manejar héroes y formulario de edición

--- src/app/pages/heroe/heroe.component.ts
+++ src/app/pages/heroe/heroe.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { HeroeModel } from 'src/app/models/heroe.model';
 
 @Component({
   selector: 'app-heroe',
@@ -7,9 +9,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeroeComponent implements OnInit {
 
+  heroe = new HeroeModel();
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  guardar(form: NgForm){
+    if(form.invalid) {
+
+      return;
+    }
+
   }
 
 }
