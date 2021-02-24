01c40fa5a623a2c086c584251e35176b8b7f75c7
Saúl Laínez <saullainez@hotmail.es>
2021-02-24 09:50:23 -0600

    Información del estado de las peticiones

--- src/app/pages/heroe/heroe.component.ts
+++ src/app/pages/heroe/heroe.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { Observable } from 'rxjs';
 import { HeroeModel } from 'src/app/models/heroe.model';
 import { HeroesService } from 'src/app/services/heroes.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-heroe',
@@ -18,19 +20,36 @@ export class HeroeComponent implements OnInit {
   }
 
   guardar(form: NgForm){
+
     if(form.invalid) {
       return;
     }
 
+    Swal.fire({
+      title: 'Espere',
+      text: 'Guardando información',
+      icon: 'info',
+      allowOutsideClick: false
+    });
+    Swal.showLoading();
+    let peticion: Observable<any>;
+    let userMessage: string;
+
     if(this.heroe.id){
-      this.heroesService.actualizarHeroe(this.heroe).subscribe(resp => {
-        console.log(resp);
-      });
+      peticion = this.heroesService.actualizarHeroe(this.heroe);
+      userMessage = 'Se actualizó correctamente';
     }else{
-      this.heroesService.crearHeroe(this.heroe).subscribe(resp => {
-        console.log(resp);
-      });
+      peticion = this.heroesService.crearHeroe(this.heroe);
+      userMessage = 'Se creó correctamente';
     }
+
+    peticion.subscribe(resp => {
+      Swal.fire({
+        title: this.heroe.nombre,
+        text: userMessage,
+        icon: 'success'
+      })
+    })
 
 
 
